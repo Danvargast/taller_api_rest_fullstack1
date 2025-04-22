4f1cef6000653e009cdb85a71e811afe333736c0
Danvargast <dan.vargast@duocuc.cl>
2025-04-22 13:52:16 -0400

    initial commit

---
+++ restapi/src/main/java/com/danvargast/tallerapirest/restapi/RestapiApplication.java
@@ -0,0 +1,13 @@
+package com.danvargast.tallerapirest.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
